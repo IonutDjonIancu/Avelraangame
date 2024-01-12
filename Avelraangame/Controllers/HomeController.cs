96aa250ee753e66fba0cf730c6df2a6fe69dcf7e
iancu ionut <iiancu85@gmail.com>
2024-01-12 20:24:06 +0100

    Added home controller. Changed cors policy to allow Vue.js application calls.

---
+++ Avelraangame/Controllers/HomeController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Avelraangame.Controllers;
+
+public class HomeController : Controller
+{
+    public IActionResult Index()
+    {
+        return Ok("All okay!");
+    }
+}
